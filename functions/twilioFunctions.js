5bf236d65d39ad0aea89180156237d3ac5906e9f
Shareef Ali <64172657+shareef945@users.noreply.github.com>
2024-03-09 15:10:10 +0000

    deploy to cloud run

--- functions/twilioFunctions.js
+++ functions/twilioFunctions.js
@@ -1,13 +1,18 @@
 const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID;
 const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
-export async function sendSMS (body) {
-     const messages = await client.messages
-        .create({
-            body: body.body,
-            from: process.env.REACT_APP_TWILIO_NUMBER,
-            to: body.to
-        }).then(message => { console.log(message)  });
-        return messages
-}
+const sendSMS = async function (body) {
+  const messages = await client.messages
+    .create({
+      body: body.body,
+      from: process.env.REACT_APP_TWILIO_NUMBER,
+      to: body.to,
+    })
+    .then((message) => {
+      console.log(message);
+    });
+  return messages;
+};
+
+export { sendSMS };
